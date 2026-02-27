2458c34a4722daab930c55fa07370b2c05ed9077
Jorge-R-V <alumno.656115@ies-azarquiel.es>
2026-02-27 01:14:57 +0100

    arreglo rutas

--- proyectoFinalDisenio/vite.config.ts
+++ proyectoFinalDisenio/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
-  base: "/DIW2/",
+  base: "/DIW/",
   build: {
     rollupOptions: {
       input: {
